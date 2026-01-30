10a4a4d9d690e8713cb0c151b96afe63df28bdd9
ulfia04 <ulfiadwi60@gmail.com>
2026-01-30 08:10:17 +0700

    pertemuan 1

---
+++ pertemuan 1/output.py
@@ -0,0 +1,21 @@
+print("my name is!")
+print("aisyah.")
+print("jakarta!")
+
+print("jurusan saya!")
+print('informatic!')
+
+print("Hello aisyah", end=" ")
+print("Saya sedang belajar")
+
+print(5)
+print(778)
+print(70000)
+
+print(5 + 7)
+print(9 * 3)
+
+print("umur saya", 17, "tahun.")
+
+
+
---
+++ pertemuan 1/tipedata.py
@@ -0,0 +1,30 @@
+a = "minuman"                # string
+b = 42                       # integer
+c = 3.1415                    # float
+d = ("garam", "merica", "cuka") # tuple
+e = ["ikan", "udang", "kepiting"] # list
+f = range(5)                 # range
+g = {"buah": "mangga", "sayur": "bayam"} # dict
+h = {"jeruk", "pepaya", "nanas"}         # set
+i = False                     # boolean
+j = frozenset({"teh", "kopi"}) # frozenset
+k = b"cemilan"                # bytes
+l = bytearray(5)              # bytearray
+m = memoryview(bytes(3))      # memoryview
+n = None                      # NoneType
+
+# Cek tipe datanya (opsional)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(i))
+print(type(j))
+print(type(k))
+print(type(l))
+print(type(m))
+print(type(n))
